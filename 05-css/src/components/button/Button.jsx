df9e5db3d845a3e66b8bc21518cf4d43d544eeb4
Mazahir <mazahirhussain354@gmail.com>
2025-11-04 19:57:43 +0500

    css done

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import buttonStyle from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div className={buttonStyle.btn}>
+        This is a Button.
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import HeaderStyle from './Header.module.css'
+const Header = () => {
+  return (
+    <div className={HeaderStyle.btn}>
+      Hello
+    </div>
+  )
+}
+
+export default Header
